bc41c9ac03adc319ef279162da72d81a906d72bc
Moluno-xiii <molunoprogress@gmail.com>
2023-11-09 08:16:23 +0100

    fetch finally worked

--- src/components/CurrentTime/AutomaticTimeZone.tsx
+++ src/components/CurrentTime/AutomaticTimeZone.tsx
@@ -14,43 +14,62 @@ const AutomaticTimeZone: React.FC = () => {
 
 const fetch_url = 'https://timeapi.io/api/TimeZone/zone?timeZone=Europe/Amsterdam'
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        setIsLoading(true);
-        const response = await fetch(`${fetch_url}`);
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        const data = await response.json();
-        setZoneData(data)
-        console.log(data)
-      } catch (err : any) {
-        setError(err.toString());
-        console.error(err);
-      } finally {
-        setIsLoading(false)
-      }
-    }
-    fetchData()
-  }, [zone, fetch_url]);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       setIsLoading(true);
+  //       const response = await fetch(`${fetch_url}`);
+  //       if (!response.ok) {
+  //         throw new Error("Failed to fetch data");
+  //       }
+  //       const data = await response.json();
+  //       setZoneData(data)
+  //       console.log(data)
+  //     } catch (err : any) {
+  //       setError(err.toString());
+  //       console.error(err);
+  //     } finally {
+  //       setIsLoading(false)
+  //     }
+  //   }
+  //   fetchData()
+  // }, [zone, fetch_url]);
 
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
 setZone(e.target.value)
 console.log(e.target.value)
+
+  }
+
+  async function fetchZone() {
+    // const res = await fetch(
+    //   "https://cors-anywhere.heroku.app.com/https://timeapi.io/api/TimeZone/zone?timeZone=Europe/Amsterdam"
+    // );
+    const res = await fetch(
+      "https://api.allorigins.win/get?url=https://timeapi.io/api/TimeZone/zone?timeZone=Europe/Amsterdam"
+    );
+    const final = await res.json();
+    console.log(final);
+    return final;
   }
   return (
+    <>
     <form>
       {/* <input type="text" /> */}
       <select name="timezone" value  ={zone || ''} onChange={handleChange}>
         {allZones.map((zone, index) => (
-            <option value={zone} key={index}>
+          <option value={zone} key={index}>
                 {zone}
             </option>
         ))}
       </select>
     </form>
+
+    <button onClick={fetchZone}>
+      click me
+    </button>
+        </>
   );
 };
 
