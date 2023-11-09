cf287eaafea979dba47840df40ba8a9736f34323
Moluno-xiii <molunoprogress@gmail.com>
2023-11-09 11:57:25 +0100

    fixed app by using new API

--- src/components/CurrentTime/AutomaticTimeZone.tsx
+++ src/components/CurrentTime/AutomaticTimeZone.tsx
@@ -1,31 +1,38 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { allZones } from "../../misc/array";
+import { allZones, newZones } from "../../misc/array";
 import Spinner from "../Loader";
 
 interface zoneDataParam {
-  timeZone: string;
-  currentLocalTime: string;
-  hasDayLightSaving: boolean;
-  isDayLightSavingActive: boolean;
+  abbreviation: string;
+  datetime: string;
+  timezone: boolean;
+  week_number: boolean;
+  day_of_week: number | null;
+  day_of_year: number | null;
+  client_ip: string
+
 }
 
 const defaultZoneData: zoneDataParam = {
-  timeZone: "",
-  currentLocalTime: "",
-  hasDayLightSaving: false,
-  isDayLightSavingActive: false,
+  abbreviation: "",
+  datetime: "",
+  timezone: false,
+  week_number: false,
+  day_of_week: null,
+  day_of_year: null,
+  client_ip: ''
 };
 
 const AutomaticTimeZone: React.FC = () => {
-  const [zone, setZone] = useState<string | null>("");
+  const [zone, setZone] = useState<string | null>("Africa/Abidjan");
   const [zoneData, setZoneData] = useState<zoneDataParam>(defaultZoneData);
   const [error, setError] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  // const fetch_url: string = `https://timeapi.io/api/Time/current/zone?timeZone=${zone}`;
+  const fetch_url: string = `http://worldtimeapi.org/api/timezone/${zone}`;
 
-  const fetch_url = `https://api.allorigins.win/get?url=https://timeapi.io/api/TimeZone/zone?timeZone=${zone}`;
+  // const fetch_url = `https://api.allorigins.win/get?url=https://timeapi.io/api/TimeZone/zone?timeZone=${zone}`;
 
   useEffect(() => {
     async function fetchData() {
@@ -36,16 +43,19 @@ const AutomaticTimeZone: React.FC = () => {
           throw new Error("Failed to fetch data");
         }
         const resData = await response.json();
-        const data = JSON.parse(resData.contents);
+        // const data = JSON.parse(resData.contents);
         setZoneData({
-          timeZone: data.timeZone,
-          currentLocalTime: data.currentLocalTime,
-          hasDayLightSaving: data.hasDayLightSaving,
-          isDayLightSavingActive: data.isDayLightSavingActive,
+          abbreviation: resData.abbreviation,
+          datetime: resData.datetime,
+          timezone: resData.timezone,
+          week_number: resData.week_number,
+          day_of_week: resData.day_of_week,
+          day_of_year: resData.day_of_year,
+          client_ip: resData.client_ip,
         });
-        console.log(zoneData.hasDayLightSaving);
-        console.log(zoneData);
-        console.log(data.contents);
+        // console.log(zoneData.hasDayLightSaving);
+        // console.log(zoneData);
+        // console.log(data.contents);
       } catch (err: any) {
         setError(err.toString());
         console.error(err);
@@ -56,6 +66,36 @@ const AutomaticTimeZone: React.FC = () => {
     fetchData();
   }, [zone,fetch_url]);
 
+  const fetchZones = async () => {
+          try {
+            setIsLoading(true);
+            setError('')
+            const response = await fetch(`${fetch_url}`);
+            if (!response.ok) {
+              throw new Error("Failed to fetch data");
+            }
+            const resData = await response.json();
+            // const data = JSON.parse(resData.contents);
+            setZoneData({
+              abbreviation: resData.abbreviation,
+              datetime: resData.datetime,
+              timezone: resData.timezone,
+              week_number: resData.week_number,
+              day_of_week: resData.day_of_week,
+              day_of_year: resData.day_of_year,
+              client_ip: resData.client_ip
+            });
+            console.log(resData.abbreviation);
+            console.log(zoneData);
+            setZoneData(resData)
+          } catch (err: any) {
+            setError(err.toString());
+            console.error(err);
+          } finally {
+            setIsLoading(false);
+          }
+  };
+
  
 
 
@@ -64,10 +104,10 @@ const AutomaticTimeZone: React.FC = () => {
     console.log(e.target.value);
   };
 
-  const { hasDayLightSaving, currentLocalTime, isDayLightSavingActive, timeZone } =
+  const { abbreviation, datetime, week_number, timezone, day_of_week, day_of_year, client_ip } =
     zoneData as zoneDataParam;
 
-  const timestamp = new Date(currentLocalTime);
+  const timestamp = new Date(datetime);
   const date: string = timestamp.toLocaleDateString(); // Extract date
   const time: string = timestamp.toLocaleTimeString();
  
@@ -76,7 +116,7 @@ const AutomaticTimeZone: React.FC = () => {
       <form>
         {/* <input type="text" /> */}
         <select name="timezone" value={zone || ""} onChange={handleChange}>
-          {allZones.map((zone, index) => (
+          {newZones.map((zone, index) => (
             <option value={zone} key={index}>
               {zone}
             </option>
@@ -86,17 +126,20 @@ const AutomaticTimeZone: React.FC = () => {
 
       {!isLoading && !error && (
         <div>
-          <p>current local time:{currentLocalTime}</p>
+          <p>current local time:{datetime}</p>
           <p>Time:{time}</p>
           <p>Date:{date}</p>
-          <p>Time zone:{timeZone}</p>
+          <p>Time zone:{timezone}</p>
           <p>
-            Daylight savings status:{hasDayLightSaving ? "active" : "inactive"}
+            week number {week_number}
           </p>
           <p>
-            Is Daylight saving active:
-            {isDayLightSavingActive ? "active" : "inactive"}
+            Abbreviation
+            {abbreviation }
           </p>
+          <p>Your IP address : {client_ip}</p>
+          <p>Day of week : {day_of_week}</p>
+          <p>Day of year : {day_of_year}</p>
           {/* <p>Is Daylight saving active:{zoneData.isDayLightSavingActive}</p> */}
         </div>
       )}
@@ -104,9 +147,9 @@ const AutomaticTimeZone: React.FC = () => {
       {isLoading && <Spinner />}
       {error && <p>{error}</p>}
 
-      {/* <button onClick={fetchZone}>
+       <button onClick={fetchZones}>
       click me
-    </button> */}
+    </button> 
     </>
   );
 };
