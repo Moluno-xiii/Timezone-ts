4d77a1453dfece804669174a9c6c53ea681e336c
Moluno-xiii <molunoprogress@gmail.com>
2023-11-09 08:35:24 +0100

    destructured data from api

--- src/components/CurrentTime/AutomaticTimeZone.tsx
+++ src/components/CurrentTime/AutomaticTimeZone.tsx
@@ -3,37 +3,53 @@ import { useState } from "react";
 import { allZones } from "../../misc/array";
 
 
+interface zoneDataParam {
+  timeZone : string;
+  currentLocalTime : string;
+  hasDayLightSaving : boolean;
+  isDayLightSavingActive : boolean;
+}
+
+
+const defaultZoneData : zoneDataParam = {
+  timeZone : '',
+  currentLocalTime : '',
+  hasDayLightSaving : false,
+  isDayLightSavingActive : false,
+}
+
 
 const AutomaticTimeZone: React.FC = () => {
   const [zone, setZone] = useState<string | null>('');
-  const [zoneData, setZoneData] = useState<string | null>('');
+  const [zoneData, setZoneData] = useState<zoneDataParam>(defaultZoneData);
   const [error, setError] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
 // const fetch_url: string = `https://timeapi.io/api/Time/current/zone?timeZone=${zone}`;
 
-const fetch_url = 'https://timeapi.io/api/TimeZone/zone?timeZone=Europe/Amsterdam'
+const fetch_url =
+   `https://api.allorigins.win/get?url=https://timeapi.io/api/TimeZone/zone?timeZone=${zone}`;
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-  //       setIsLoading(true);
-  //       const response = await fetch(`${fetch_url}`);
-  //       if (!response.ok) {
-  //         throw new Error("Failed to fetch data");
-  //       }
-  //       const data = await response.json();
-  //       setZoneData(data)
-  //       console.log(data)
-  //     } catch (err : any) {
-  //       setError(err.toString());
-  //       console.error(err);
-  //     } finally {
-  //       setIsLoading(false)
-  //     }
-  //   }
-  //   fetchData()
-  // }, [zone, fetch_url]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`${fetch_url}`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        setZoneData(data)
+        console.log(data)
+      } catch (err : any) {
+        setError(err.toString());
+        console.error(err);
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+  }, [zone, fetch_url]);
 
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -42,17 +58,27 @@ console.log(e.target.value)
 
   }
 
-  async function fetchZone() {
-    // const res = await fetch(
-    //   "https://cors-anywhere.heroku.app.com/https://timeapi.io/api/TimeZone/zone?timeZone=Europe/Amsterdam"
-    // );
-    const res = await fetch(
-      "https://api.allorigins.win/get?url=https://timeapi.io/api/TimeZone/zone?timeZone=Europe/Amsterdam"
-    );
-    const final = await res.json();
-    console.log(final);
-    return final;
-  }
+
+  const {
+    hasDayLightSaving,
+    timeZone,
+    currentLocalTime,
+    isDayLightSavingActive,
+  } = zoneData as zoneDataParam
+
+
+   const timestamp = new Date(currentLocalTime);
+   const date: string = timestamp.toLocaleDateString(); // Extract date
+   const time: string = timestamp.toLocaleTimeString();
+  // async function fetchZone() {
+
+  //   const res = await fetch(
+  //     "https://api.allorigins.win/get?url=https://timeapi.io/api/TimeZone/zone?timeZone=Europe/Amsterdam"
+  //   );
+  //   const final = await res.json();
+  //   console.log(final);
+  //   return final;
+  // }
   return (
     <>
     <form>
@@ -65,10 +91,11 @@ console.log(e.target.value)
         ))}
       </select>
     </form>
+    <p></p>
 
-    <button onClick={fetchZone}>
+    {/* <button onClick={fetchZone}>
       click me
-    </button>
+    </button> */}
         </>
   );
 };
