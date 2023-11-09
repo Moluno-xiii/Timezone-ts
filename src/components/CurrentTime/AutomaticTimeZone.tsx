48821c6004507f2cda9b4c790ab1318722e49991
Moluno-xiii <molunoprogress@gmail.com>
2023-11-09 02:14:17 +0100

    added a few features

--- src/components/CurrentTime/AutomaticTimeZone.tsx
+++ src/components/CurrentTime/AutomaticTimeZone.tsx
@@ -1,10 +1,47 @@
-import React from 'react'
-const AutomaticTimeZone: React.FC = () => {
-    return (
-        <div>
-            select your time zone
-        </div>
-    )
-}
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { allZones } from "../../misc/array";
 
-export default AutomaticTimeZone
+
+
+const AutomaticTimeZone: React.FC = () => {
+  const [zone, setZone] = useState<string | null>(null);
+  const [error, setError] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+const fetch_url: string = `https://timeapi.io/api/Time/current/zone?timeZone=${zone}`;
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`${fetch_url}`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        console.log(data)
+      } catch (err : any) {
+        setError(err.toString());
+        console.error(err);
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+  }, []);
+  return (
+    <form>
+      <input type="text" />
+      <select name="timezone" value="">
+        {allZones.map((zone, index) => (
+            <option value={zone} key={index}>
+                {zone}
+            </option>
+        ))}
+      </select>
+    </form>
+  );
+};
+
+export default AutomaticTimeZone;
