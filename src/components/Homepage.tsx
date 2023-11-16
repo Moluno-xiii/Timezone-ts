d0a81e9c6bfdbf70e0ea3e51534b68bc600f0f2e
Moluno-xiii <molunoprogress@gmail.com>
2023-11-16 14:31:09 +0100

    homepage flex  finished

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "./Homepage.module.css"
 
 const Homepage: React.FC = () => {
   return (
-    <ul className="homepage">
-      <li className="items">
+    <ul className={styles.homepage}>
+      <li className={styles.items}>
         <NavLink to="current time"> Get Current Time </NavLink>
       </li>
-      <li className="items">
+      <li className={styles.items}>
         <NavLink to="Available zones"> See All Available time zones </NavLink>
       </li>
-      <li className="items">
+      <li className={styles.items}>
         <NavLink to="conversion">convert to another time zone</NavLink>
       </li>
-      <li className="items">
+      <li className={styles.items}>
         <NavLink to="calculate">calculate time</NavLink>
       </li>
     </ul>
   );
 };
+
+
 
 export default Homepage;
