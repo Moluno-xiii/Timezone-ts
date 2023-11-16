d09cd63b7573d347d3aeac1984d1a79dc32ad04d
Moluno-xiii <molunoprogress@gmail.com>
2023-11-16 13:54:06 +0100

    added flex to homepage

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -4,16 +4,16 @@ import { NavLink } from "react-router-dom";
 const Homepage: React.FC = () => {
   return (
     <ul className="homepage">
-      <li>
+      <li className="items">
         <NavLink to="current time"> Get Current Time </NavLink>
       </li>
-      <li>
+      <li className="items">
         <NavLink to="Available zones"> See All Available time zones </NavLink>
       </li>
-      <li>
+      <li className="items">
         <NavLink to="conversion">convert to another time zone</NavLink>
       </li>
-      <li>
+      <li className="items">
         <NavLink to="calculate">calculate time</NavLink>
       </li>
     </ul>
